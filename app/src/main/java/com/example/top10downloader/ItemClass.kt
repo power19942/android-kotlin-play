3a09310fa58d38622dba7dd39a16948df14ba366
omar jakmira <poweromar19942@gmail.com>
2019-11-09 23:24:54 +0200

    pick image from camera

--- app/src/main/java/com/example/top10downloader/ItemClass.kt
+++
@@ -1,7 +0,0 @@
-package com.example.top10downloader
-
-import android.widget.BaseAdapter
-
-class ItemClass(var title:String,var image:Int){
-
-}
--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.top10downloader
 
 import android.app.Dialog
+import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.drawable.AnimationDrawable
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -13,28 +16,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var list = findViewById<ListView>(R.id.list_view)
-        var item = ArrayList<ItemClass>()
-        item.add(ItemClass("Android List",R.drawable.img))
-        item.add(ItemClass("stupid list",R.drawable.img2))
-        item.add(ItemClass("Android List",R.drawable.img))
-        item.add(ItemClass("stupid list",R.drawable.img2))
+        var openBtn : Button = findViewById(R.id.btn)
 
-        var addater = CustomAdapter(this@MainActivity,item);
-        list.adapter = addater
-        list.onItemClickListener = AdapterView.OnItemClickListener{parent,view,pos,id->
-            val alert = Dialog(this@MainActivity)
-            alert.setContentView(R.layout.custom_alert)
-            val title = alert.findViewById<TextView>(R.id.dialog_txt)
-            val img = alert.findViewById<ImageView>(R.id.dialog_img)
-            val btn = alert.findViewById<Button>(R.id.dialog_button)
-            val item: ItemClass? = addater.getItem(pos)
-            title.text = item?.title
-            img.setImageResource(item?.image ?: R.drawable.img2)
-            btn.setOnClickListener{
-                alert.dismiss()
-            }
-            alert.show()
+        openBtn.setOnClickListener{it
+            val intent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent,548)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 548){
+            var img = data!!.extras?.get("data") as Bitmap
+            val image = findViewById<ImageView>(R.id.imageView2)
+            image.setImageBitmap(img)
         }
     }
 
