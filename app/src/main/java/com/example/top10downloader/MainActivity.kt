292324cc5d5f148966072da841ac9c0ff2e56e74
omar jakmira <poweromar19942@gmail.com>
2019-11-09 23:32:13 +0200

    make call

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.AnimationDrawable
 import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -19,19 +20,12 @@ class MainActivity : AppCompatActivity() {
         var openBtn : Button = findViewById(R.id.btn)
 
         openBtn.setOnClickListener{it
-            val intent = Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(intent,548)
+            val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel","54545121",null))
+            startActivity(intent)
         }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 548){
-            var imgUri = data?.data
-            val image = findViewById<ImageView>(R.id.imageView2)
-            image.setImageURI(imgUri)
-        }
-    }
+
 
 }
