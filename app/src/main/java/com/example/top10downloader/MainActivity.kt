4eb81455971538bb3577996d2a7dda7c6eed451f
omar jakmira <poweromar19942@gmail.com>
2019-11-10 22:26:49 +0200

    layout manager

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -57,8 +58,21 @@ class MainActivity : AppCompatActivity() {
         item.add(CustomItem("Android List",R.drawable.img))
         item.add(CustomItem("stupid list",R.drawable.img2))
         var adapter = CustomAdapter(item,applicationContext)
-        list.layoutManager = GridLayoutManager(applicationContext,4)
+//        list.layoutManager = GridLayoutManager(applicationContext,4)
+        list.layoutManager = LinearLayoutManager(applicationContext)
         list.adapter = adapter
+        var type:Int = 0
+        btn.setOnClickListener {
+            if (type==0){
+                list.layoutManager = GridLayoutManager(applicationContext,4)
+                type =1
+            }
+            else{
+                type=0
+                list.layoutManager = LinearLayoutManager(applicationContext)
+            }
+
+        }
     }
 
 }
