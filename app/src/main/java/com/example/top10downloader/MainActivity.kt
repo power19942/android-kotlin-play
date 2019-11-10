ed0cc9143e06a90e899b33f27111b6c7fa94c633
omar jakmira <poweromar19942@gmail.com>
2019-11-10 17:50:58 +0200

    empty

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -13,29 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var list = findViewById<ListView>(R.id.list_view)
-        var item = ArrayList<ItemClass>()
-        item.add(ItemClass("Android List",R.drawable.img))
-        item.add(ItemClass("stupid list",R.drawable.img2))
-        item.add(ItemClass("Android List",R.drawable.img))
-        item.add(ItemClass("stupid list",R.drawable.img2))
 
-        var addater = CustomAdapter(this@MainActivity,item);
-        list.adapter = addater
-        list.onItemClickListener = AdapterView.OnItemClickListener{parent,view,pos,id->
-            val alert = Dialog(this@MainActivity)
-            alert.setContentView(R.layout.custom_alert)
-            val title = alert.findViewById<TextView>(R.id.dialog_txt)
-            val img = alert.findViewById<ImageView>(R.id.dialog_img)
-            val btn = alert.findViewById<Button>(R.id.dialog_button)
-            val item: ItemClass? = addater.getItem(pos)
-            title.text = item?.title
-            img.setImageResource(item?.image ?: R.drawable.img2)
-            btn.setOnClickListener{
-                alert.dismiss()
-            }
-            alert.show()
-        }
     }
 
 }
