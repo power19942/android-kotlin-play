2d5078566f769ce981843b1ce9b6673ba82d3bda
omar jakmira <poweromar19942@gmail.com>
2019-11-10 23:08:57 +0200

    bottom navigation

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var game = play_fragment()
+        var test = settings_fragment()
+        var posts = posts_fragment()
+        nav.selectedItemId = R.id.home
+        var m = supportFragmentManager.beginTransaction()
+        m.replace(R.id.holder,posts)
+        m.commit()
+
+        nav.setOnNavigationItemSelectedListener { item->
+
+            when(item.itemId){
+                R.id.game -> {
+                    var manager = supportFragmentManager.beginTransaction()
+                    manager.replace(R.id.holder,game)
+                    manager.commit()
+                    true
+                }
+                R.id.test -> {
+                    var manager = supportFragmentManager.beginTransaction()
+                    manager.replace(R.id.holder,test)
+                    manager.commit()
+                    true
+                }
+                R.id.home -> {
+                    var manager = supportFragmentManager.beginTransaction()
+                    manager.replace(R.id.holder,posts)
+                    manager.commit()
+                    true
+                }
+                else -> true
+            }
+        }
 
     }
 
